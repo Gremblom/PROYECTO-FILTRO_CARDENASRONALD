a1645d32d56eaa31d63f8332ada6d8ec65de42c0
Rodaca <leapardo4@hotmail.com>
2023-10-09 08:36:44 -0500

    style: :lipstick: pantalla de carga
    se crea la pantalla de carga se suben el fondo y el logo se da estructura

---
+++ frontend/kairo-app/src/components/pgCarga.jsx
@@ -0,0 +1,9 @@
+import logo from "../asset/KARIO_LOGO.png";
+
+export default function carga(){
+    return(
+        <div className="logocarga">
+            <img src={logo} width={200} alt="" />
+        </div>
+    )
+}
