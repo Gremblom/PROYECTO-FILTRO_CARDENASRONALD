61067da0195ef7330a8913aa0a95911116ede37f
Jsanchez32 <juanandressanchezrey@gmail.com>
2023-10-10 08:56:07 -0500

    feat: :sparkles: Login user, rutas protegidas

--- backend/controllers/login.controller.js
+++ backend/controllers/login.controller.js
@@ -7,13 +7,13 @@ const login = async (req, res) => {
         const db = await conection();
         const coleccion = db.collection('usuario');
         const user = await coleccion.findOne({ Correo: req.body.correo })
-
         if (user) {
             const validatePassword = bcryptjs.compareSync(req.body.password, user.Password)
             if (validatePassword) {
                 const token = await generateJWT(user._id)
                 return res.send({
                     msg: 'Iniciando Sesion',
+                    user:user,
                     token: token
                 });
             }
@@ -29,33 +29,6 @@ const login = async (req, res) => {
     }
 }
 
-
-const register = async (req, res) => {
-    try {
-        const db = await conection();
-        const coleccion = db.collection('usuario');
-        const emailExist = await coleccion.findOne({ Correo: req.body.correo });
-        if (emailExist) {
-            return res.status(404).send('Email ya existe');
-        }
-        else {
-            const { Username, Correo, Password } = req.body;
-            const salt = bcryptjs.genSaltSync();
-            const encriptPassword = bcryptjs.hashSync(Password, salt);
-            const data = {
-                Username,
-                Correo,
-                Password: encriptPassword
-            }
-            const response = await coleccion.insertOne(data);
-            res.status(200).send(response);
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
 export {
-    login,
-    register
+    login
 } 
--- frontend/kairo-app/src/components/login.jsx
+++ frontend/kairo-app/src/components/login.jsx
@@ -1,17 +1,44 @@
+import React,{useState} from 'react';
 import logo from "../asset/KARIO_LOGO.png";
-import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Carga from "./pgCarga";
+import Swal from 'sweetalert2'
+import axios from "axios";
+
 
 
 export default function Login(){
+    const [password,setPassword] = useState("");
+    const [correo,setCorreo] = useState("");
+
     const history = useHistory();
-    const redirigirANuevaRuta = () => {
-        history.push('/Bienvenido');
-    };
+    const enterLogin = async()=>{
+        try {
+            const login = await axios.post(`http://localhost:7234/auth/login`,{
+                correo,password
+            }) 
+            localStorage.setItem('token',login.data.token);
+            localStorage.setItem('userName',login.data.user.Username);
+            localStorage.setItem('imgUser',login.data.user.Img);
+            Swal.fire({
+                title: 'Iniciando Sesion',
+                text: 'Datos validos',
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 800
+              })
+            history.push('/bienvenido')
+        } catch (error) {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Email o Password incorrecta',
+                icon: 'error',
+                showConfirmButton: false,
+                timer: 800
+              })
+        }
+    }
     return(
-        
-        
         <div className="login">
             <img src={logo} width={100} alt="" />
             <h2 className="media">MEDIA</h2>
@@ -19,10 +46,10 @@ export default function Login(){
             <p>Por favor ingresa los siguientes datos para ingresar a la plataforma</p>
             <form>
                 <label htmlFor="">Usuario</label>
-                <input type="text" />
+                <input type="email" value={correo} onChange={(e)=>setCorreo(e.target.value)} />
                 <label htmlFor="">Contraseña</label>
-                <input type="text" />
-                <button type="submit" onClick={redirigirANuevaRuta}>Ingresar al panel</button>
+                <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+                <button type="submit" onClick={(e)=>{e.preventDefault();enterLogin()}} >Ingresar al panel</button>
             </form>
             <h6>Tienes problemas para ingresar? Por favor contactarse con asistencia técnica lo más pronto posible</h6>
         </div>
