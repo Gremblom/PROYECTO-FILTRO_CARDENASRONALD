e33a6f8a5b1ff45954855b83846e5e06fb2b4f48
Gremblom <cdflorezs@gmail.com>
2023-10-10 09:29:55 -0500

    ✨ módulo reportes completo y funcional

--- frontend/kairo-app/src/App.js
+++ frontend/kairo-app/src/App.js
@@ -4,6 +4,7 @@ import Carga from './components/pgCarga';
 import Login from './components/login';
 import Bienvenido from './components/bienvenida';
 import Ayuda from './components/ayuda/Ayuda';
+import Reportes from './components/reportes';
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
       <Router>
           <Route exact path="/" render={() => <Carga ruta="login" />} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/bienvenido" component={Bienvenido}/>
-          <Route exact path="/ayuda" component={Ayuda}/>
-          <Route exact path="/cargando" render={() => <Carga ruta="main" />}/>
+          <Route exact path="/bienvenido" component={Bienvenido} />
+          <Route exact path="/cargando" render={() => <Carga ruta="main" />} />
+          <Route exact path="/ayuda" component={Ayuda} />
+          <Route exact path="/reportes" component={Reportes} />
       </Router>
     </div>
   );
