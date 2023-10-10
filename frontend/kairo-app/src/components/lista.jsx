e66313d228c92ab378691a853381dd35974c0256
Rodacay <leapardo4@gmail.com>
2023-10-10 13:53:21 -0500

    style: :lipstick: estilo de itene de la lista
    se crea los estio y ma queta la tabla de panel

--- frontend/kairo-app/src/components/lista.jsx
+++ frontend/kairo-app/src/components/lista.jsx
@@ -1,48 +1,102 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-export default function List (){
-    return(
+
+export default function List() {
+    return (
         <div className="list">
-            <table className="tabla">
-                <thead>
-                    <tr>
-                        <td className="negrilla">Indicador</td>
-                        <td className="negrilla">Descripcion</td>
-                        <td className="negrilla">Categoria</td>
-                        <td className="negrilla">Fecha de Inicio</td>
-                        <td className="negrilla">Fecha de Terminación</td>
-                        <td className="negrilla">Formula</td>
-                        <td className="negrilla">Frecuencia</td>
-                        <td className="negrilla">Cumplimiento</td>
-                        <td className="negrilla">Area</td>
-                    </tr>
-                </thead>
-                <tbody className="">
-                    <tr className="">
-                        <td>Modelo3D</td>
-                        <td>interes por dise....</td>
-                        <td>Baja</td>
-                        <td>12/05/21</td>
-                        <td>12/12/21</td>
-                        <td>Meg.Agil</td>
-                        <td>1/4</td>
-                        <td>31%</td>
-                        <td>Marketing</td>
-                        <td></td>
-                    </tr>
-                    <tr className="">
-                        <td>Modelo3D</td>
-                        <td>interes por dise....</td>
-                        <td>Baja</td>
-                        <td>12/05/21</td>
-                        <td>12/12/21</td>
-                        <td>Meg.Agil</td>
-                        <td>1/4</td>
-                        <td>31%</td>
-                        <td>Marketing</td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="header">
+                <div className="negrilla">Indicador</div>
+                <div className="negrilla">Descripcion</div>
+                <div className="negrilla">Categoria</div>
+                <div className="negrilla">Fecha de Inicio</div>
+                <div className="negrilla">Fecha de Terminación</div>
+                <div className="negrilla">Formula</div>
+                <div className="negrilla">Frecuencia</div>
+                <div className="negrilla">Cumplimiento</div>
+                <div className="negrilla">Area</div>
+            </div>
+
+            <div className='items'>
+                <div className="item">
+                    <div>Modelo3D</div>
+                    <div>interes por dise....</div>
+                    <div>Baja</div>
+                    <div>12/05/21</div>
+                    <div>12/12/21</div>
+                    <div>Meg.Agil</div>
+                    <div>1/4</div>
+                    <div>31%</div>
+                    <div>Marketing</div>
+                </div>
+                <div className='faBarss'>
+                    <FontAwesomeIcon icon={faBars} size="2xl" style={{color: "#000000", }} />
+                </div>
+            </div>
+            <div className='items'>
+                <div className="item">
+                    <div>Modelo3D</div>
+                    <div>interes por dise....</div>
+                    <div>Baja</div>
+                    <div>12/05/21</div>
+                    <div>12/12/21</div>
+                    <div>Meg.Agil</div>
+                    <div>1/4</div>
+                    <div>31%</div>
+                    <div>Marketing</div>
+                </div>
+                <div className='faBarss'>
+                    <FontAwesomeIcon icon={faBars} size="2xl" style={{color: "#000000", }} />
+                </div>
+            </div>
+            <div className='items'>
+                <div className="item">
+                    <div>Modelo3D</div>
+                    <div>interes por dise....</div>
+                    <div>Baja</div>
+                    <div>12/05/21</div>
+                    <div>12/12/21</div>
+                    <div>Meg.Agil</div>
+                    <div>1/4</div>
+                    <div>31%</div>
+                    <div>Marketing</div>
+                </div>
+                <div className='faBarss'>
+                    <FontAwesomeIcon icon={faBars} size="2xl" style={{color: "#000000", }} />
+                </div>
+            </div>
+            <div className='items'>
+                <div className="item">
+                    <div>Modelo3D</div>
+                    <div>interes por dise....</div>
+                    <div>Baja</div>
+                    <div>12/05/21</div>
+                    <div>12/12/21</div>
+                    <div>Meg.Agil</div>
+                    <div>1/4</div>
+                    <div>31%</div>
+                    <div>Marketing</div>
+                </div>
+                <div className='faBarss'>
+                    <FontAwesomeIcon icon={faBars} size="2xl" style={{color: "#000000", }} />
+                </div>
+            </div><div className='items'>
+                <div className="item">
+                    <div>Modelo3D</div>
+                    <div>interes por dise....</div>
+                    <div>Baja</div>
+                    <div>12/05/21</div>
+                    <div>12/12/21</div>
+                    <div>Meg.Agil</div>
+                    <div>1/4</div>
+                    <div>31%</div>
+                    <div>Marketing</div>
+                </div>
+                <div className='faBarss'>
+                    <FontAwesomeIcon icon={faBars} size="2xl" style={{color: "#000000", }} />
+                </div>
+            </div>
+            
         </div>
     )
 }
